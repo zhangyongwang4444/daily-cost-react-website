93a58f1a16f865fdd2f08c3e47812a452dcf008d
zyw <20225614@qq.com>
2020-06-25 11:42:26 +0800

    deploy

---
+++ precache-manifest.a5955c140c997f4ed78161bd3cf9f314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e42e7894b5a56fcde0fd6adb359e2c",
+    "url": "/daily-cost-react-website/index.html"
+  },
+  {
+    "revision": "cf086f471679c44b9c60",
+    "url": "/daily-cost-react-website/static/css/main.9af6a88d.chunk.css"
+  },
+  {
+    "revision": "a0c9053b7ec45d5a9f93",
+    "url": "/daily-cost-react-website/static/js/2.558f75fd.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/daily-cost-react-website/static/js/2.558f75fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf086f471679c44b9c60",
+    "url": "/daily-cost-react-website/static/js/main.ada86aa4.chunk.js"
+  },
+  {
+    "revision": "3594bd8496c49d15dbf1",
+    "url": "/daily-cost-react-website/static/js/runtime-main.c5511347.js"
+  }
+]);
